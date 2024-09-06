fddc401551d7f68b54b1db04cf345af42e941069
Tarun Mathur <tarun.mathur01@infosys.com>
2024-09-06 12:50:01 +0530

    CounterApp

---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import style from "./Counter.module.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className={style.container}>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
